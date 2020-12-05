82721992922bfc42645984d1ab3d10791123b037
shahriar100 <shahriar.hasan100@gmail.com>
2020-12-05 19:20:46 +0600

    Completed Admin.py

--- quiz_api_project/quiz/urls.py
+++ quiz_api_project/quiz/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'quiz'
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Quiz.as_view(), name='home'),
 ]
--- quiz_api_project/quiz/views.py
+++ quiz_api_project/quiz/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    context = {}
-    return render(request, '<h1>ajdja</h1>', context)
+class Quiz(request):
+    pass
